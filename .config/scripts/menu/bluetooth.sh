2b34cd606c53b3e859afd1e1d4f879a2e39cd288
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2026-07-17 20:05:30 +0300

    Commit

--- .config/scripts/menu/bluetooth.sh
+++ .config/scripts/menu/bluetooth.sh
@@ -3,6 +3,6 @@
 if [ "$1" = "--status" ]; then
     dmenu-bluetooth --status
 else
-    export DMENU_BLUETOOTH_LAUNCHER="rofi -dmenu"
+    export DMENU_BLUETOOTH_LAUNCHER="rofi -dmenu -i -theme ~/.config/rofi/dmenu.rasi"
     dmenu-bluetooth "$@"
 fi
