b25ff6c8375813b37baa1e5f973b4f9b5f18214e
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2026-07-17 16:46:59 +0300

    Minor fixes

--- archinstall/post_install.sh
+++ archinstall/post_install.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 echo "Creating Snapper configs..."
-for conf in root home; do
-    if ! snapper -c "$conf" list &>/dev/null; then
-        snapper -c "$conf" create-config "/${conf#home}"
-    fi
-done
+if ! snapper -c root list &>/dev/null; then
+    snapper -c root create-config /
+fi
+if ! snapper -c home list &>/dev/null; then
+    snapper -c home create-config /
+fi
 
 echo "Starting Snapper timers..."
 systemctl enable --now snapper-timeline.timer snapper-cleanup.timer
@@ -18,6 +19,7 @@ systemctl enable --now grub-btrfsd
 echo "Remaking GRUB config..."
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "Setup US/RU keyboard layout..."
 localectl set-keymap us
 localectl set-x11-keymap us,ru "" "" grp:alt_shift_toggle
 
