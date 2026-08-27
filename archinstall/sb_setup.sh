13967d5f512823bc12caa9e7e6cfcc6decd6a5ef
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2026-08-27 17:23:55 +0300

    minor fix

--- archinstall/sb_setup.sh
+++ archinstall/sb_setup.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --modules="tpm" --disable-shim-lock
 grub-mkconfig -o /boot/grub/grub.cfg
 
 sbctl create-keys
